b77aa62cac66369266cf8b1c7f3019b7a4ad9ba6
Ngakan Gandhi <n.nyoman.gandhi@gmail.com>
2025-03-28 10:27:22 +0700

    add example to construct json

--- examples/serde-json-1/src/main.rs
+++ examples/serde-json-1/src/main.rs
@@ -1,5 +1,5 @@
 use serde::{Deserialize, Serialize};
-use serde_json::{Result, Value};
+use serde_json::{Result, Value, json};
 
 fn untyped_example() -> Result<()> {
     // Some JSON input data as a &str which might come
@@ -56,7 +56,25 @@ fn typed_example() -> Result<()> {
     Ok(())
 }
 
+fn construct_json() {
+    // the type of `john` is `serde_json::Value`
+    let john = json!({
+        "name": "John Doe",
+        "age": 43,
+        "phones": [
+            "+44 1234567",
+            "+44 2345678"
+        ]
+    });
+
+    println!("first phone number: {}", john["phones"][0]);
+
+    // Convert to a string of JSON and print it out
+    println!("{}", john.to_string());
+}
+
 fn main() {
     let _ = untyped_example();
     let _ = typed_example();
+    construct_json();
 }
