538789869ac5c8bfcd7f1587b36ce8fc6e643705
Ngakan Gandhi <n.nyoman.gandhi@gmail.com>
2025-03-31 15:21:39 +0700

    add more examples

--- examples/reqwest-spotify/src/main.rs
+++ examples/reqwest-spotify/src/main.rs
@@ -4,17 +4,71 @@ use reqwest::{
     self,
     header::{ACCEPT, AUTHORIZATION, CONTENT_TYPE},
 };
-use serde::Deserialize;
+use serde::{Deserialize, Serialize};
 
 const CLIENT_ID: &str = "8dcf83db60d94d638572e591695c5945";
 const CLIENT_SECRET: &str = "3eb6ceb5b8674ed5934075e7c55fa88d";
 
 #[allow(dead_code)]
 #[derive(Debug, Deserialize)]
-struct Token {
+struct AccessToken {
     access_token: String,
     expires_in: i32,
     token_type: String,
+}
+
+#[derive(Serialize, Deserialize, Debug)]
+struct ExternalUrls {
+    spotify: String,
+}
+
+#[derive(Serialize, Deserialize, Debug)]
+struct Artist {
+    name: String,
+    external_urls: ExternalUrls,
+}
+
+#[derive(Serialize, Deserialize, Debug)]
+struct Album {
+    name: String,
+    artists: Vec<Artist>,
+    external_urls: ExternalUrls,
+}
+
+#[derive(Serialize, Deserialize, Debug)]
+struct Track {
+    name: String,
+    href: String,
+    popularity: u32,
+    album: Album,
+    external_urls: ExternalUrls,
+}
+
+#[derive(Serialize, Deserialize, Debug)]
+struct Items<T> {
+    items: Vec<T>,
+}
+
+#[derive(Serialize, Deserialize, Debug)]
+struct APIResponse {
+    tracks: Items<Track>,
+}
+
+fn print_tracks(tracks: Vec<&Track>) {
+    for track in tracks {
+        println!("name: {}", track.name);
+        println!("album: {}", track.album.name);
+        println!(
+            "artists: {}",
+            track
+                .album
+                .artists
+                .iter()
+                .map(|artist| artist.name.to_string())
+                .collect::<String>()
+        );
+        println!("urls: {}", track.external_urls.spotify);
+    }
 }
 
 async fn get_token() -> Result<String, Box<dyn std::error::Error>> {
@@ -41,7 +95,7 @@ async fn main() {
     let tok_resp = get_token().await;
     let auth_token = match tok_resp {
         Ok(x) => {
-            let token: Token = serde_json::from_str(&x).unwrap();
+            let token: AccessToken = serde_json::from_str(&x).unwrap();
             token.access_token
         }
         Err(_) => String::from("token error"),
@@ -64,12 +118,20 @@ async fn main() {
         .await
         .unwrap();
 
+    // println!("{:#?}", response.json::<serde_json::Value>().await);
+
     match response.status() {
         reqwest::StatusCode::OK => {
-            println!("{:#?}", response.json::<serde_json::Value>().await);
+            match response.json::<APIResponse>().await {
+                Ok(parsed) => print_tracks(parsed.tracks.items.iter().collect()),
+                Err(_) => println!("The response did not match the shape we expected."),
+            };
         }
-        _ => {
-            panic!("Something unexpected happened");
+        reqwest::StatusCode::UNAUTHORIZED => {
+            println!("Need to grab a new token");
+        }
+        other => {
+            panic!("Something unexpected happened {:?}", other);
         }
     }
 }
