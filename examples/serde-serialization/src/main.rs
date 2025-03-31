6fc60c8577fd0586766549fedba13de2acb26d9d
Ngakan Gandhi <n.nyoman.gandhi@gmail.com>
2025-03-31 17:07:58 +0700

    add more examples

--- examples/serde-serialization/src/main.rs
+++ examples/serde-serialization/src/main.rs
@@ -8,6 +8,7 @@ struct User {
 }
 
 fn main() {
+    // Serialize from struct
     let user = User {
         name: String::from("Sarah"),
         age: 32,
@@ -21,4 +22,51 @@ fn main() {
     // Deserialize
     let deserialized: User = serde_json::from_str(&serialized).unwrap();
     println!("Deserialized: {:?}", deserialized);
+
+    // Deserialize from json string
+    let data_invalid = r#"
+    {
+        "name": "John Doe",
+        "age": "twenty",
+        "email": "john@example.com"
+    }
+    "#;
+
+    let result: Result<User, _> = serde_json::from_str(&data_invalid);
+
+    match result {
+        Ok(_) => println!("Successfully deserialized data"),
+        Err(err) => {
+            println!("We ran into an error: {}", err);
+            match err.classify() {
+                serde_json::error::Category::Io => println!("Problem reading file"),
+                serde_json::error::Category::Syntax => println!("Problem with JSON syntax"),
+                serde_json::error::Category::Data => println!("Problem with data"),
+                serde_json::error::Category::Eof => println!("Unexpected end of file"),
+            }
+        }
+    }
+
+    let data_valid = r#"
+    {
+        "name": "Gandhi Ngakan",
+        "age": 31,
+        "email": "ngakan.gandhi@sampoerna.com"
+    }
+    "#;
+
+    let result: Result<User, _> = serde_json::from_str(&data_valid);
+
+    match result {
+        Ok(res) => println!("Successfully deserialized data: {:#?}", res),
+        Err(err) => {
+            println!("We ran into an error: {}", err);
+            match err.classify() {
+                serde_json::error::Category::Io => println!("Problem reading file"),
+                serde_json::error::Category::Syntax => println!("Problem with JSON syntax"),
+                serde_json::error::Category::Data => println!("Problem with data"),
+                serde_json::error::Category::Eof => println!("Unexpected end of file"),
+            }
+        }
+    }
 }
