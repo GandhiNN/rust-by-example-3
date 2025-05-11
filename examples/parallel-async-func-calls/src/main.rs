97b0ccf7c738bee7afde8fe23aee92a9e7cc4770
Ngakan Gandhi <n.nyoman.gandhi@gmail.com>
2025-05-11 15:38:13 +0700

    feat: rebase

--- examples/parallel-async-func-calls/src/main.rs
+++ examples/parallel-async-func-calls/src/main.rs
@@ -1,3 +1,9 @@
+use futures::future::join_all;
+use tokio::join;
+use tokio::time::Duration;
+
+async fn fetch_data_from_url(url: &str) -> String {}
+
 fn main() {
     println!("Hello, world!");
 }
